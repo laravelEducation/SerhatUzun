d444b933ad34dcc8e667bb9186df4757e766e8f6
serhat42 <serhatuzun_02@hotmail.com>
2021-08-14 15:03:24 +0300

    e-ticaret

---
+++ Eticaret Projesi/resources/views/kategori.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title','Kategori')
+
+@section('content')
+
+    <h1>Kategori Sayfası<h1>
+
+
+@endsection
---
+++ Eticaret Projesi/resources/views/urun.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title','Ürün')
+
+@section('content')
+
+    <h1>Ürün Sayfası<h1>
+
+
+@endsection
